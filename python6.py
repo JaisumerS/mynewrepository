2e4b5ebc3a514da7a6857aaf44690006c10755c1
Jaisumer Sandhu <144252282+JaisumerS@users.noreply.github.com>
2025-01-28 01:19:14 +0000

    I hope this works

--- python6.py
+++ python6.py
@@ -1,7 +1,15 @@
 from github import Github
 from secrets import auth
 
-print(auth)
 # or using an access token
 g = Github(auth)
 repo = g.get_repo("JaisumerS/mynewrepository")
+
+print(list(repo.get_branches()))
+pulls = repo.get_pulls(state='closed', sort='created', base='main')
+for pr in pulls:
+   print(pr)
+
+commits = repo.get_commits()
+for commit in commits:
+    print(commit.commit.message)
